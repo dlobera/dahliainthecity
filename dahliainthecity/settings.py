f3f0fc6792f5de541ec58aa4d8df5af7f1345098
dlobera <delobera@gmail.com>
2022-08-10 13:57:02 -0500

    Config deployment

--- dahliainthecity/settings.py
+++ dahliainthecity/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(ccsm5$@@5c4aco59yiiq4001$bhdd6a&fsu5$8$cp2@w@egy1'
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = env('DEBUG') == 'True' 
 
 ALLOWED_HOSTS = []
